8111889f9081956129a4f9aca236c64ea03f1c82
Jason Rushton <github.com@schtuph.com>
2014-11-03 16:22:48 -0700

    the data

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+	langs: require('./data/langs'),
+	worlds: require('./data/world_names'),
+	objective_names: require('./data/objective_names'),
+	objective_types: require('./data/objective_types'),
+	objective_meta: require('./data/objective_meta'),
+	objective_labels: require('./data/objective_labels'),
+	objective_map: require('./data/objective_map'),
+};
