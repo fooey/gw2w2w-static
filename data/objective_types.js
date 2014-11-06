05e2b94b0c04200b0929ba6dd7098fbff53c2dd1
Jason Rushton <github.com@schtuph.com>
2014-11-05 21:17:42 -0700

    include id in objective type objects

--- data/objective_types.js
+++ data/objective_types.js
@@ -1,11 +1,11 @@
 module.exports = {
-	"1": {"timer": 1, "value": 35, "name": "castle"},
-	"2": {"timer": 1, "value": 25, "name": "keep"},
-	"3": {"timer": 1, "value": 10, "name": "tower"},
-	"4": {"timer": 1, "value": 5, "name": "camp"},
-	"5": {"timer": 0, "value": 0, "name": "temple"},
-	"6": {"timer": 0, "value": 0, "name": "hollow"},
-	"7": {"timer": 0, "value": 0, "name": "estate"},
-	"8": {"timer": 0, "value": 0, "name": "overlook"},
-	"9": {"timer": 0, "value": 0, "name": "ascent"}
+	"1": {"id": 1, "timer": 1, "value": 35, "name": "castle"},
+	"2": {"id": 2, "timer": 1, "value": 25, "name": "keep"},
+	"3": {"id": 3, "timer": 1, "value": 10, "name": "tower"},
+	"4": {"id": 4, "timer": 1, "value": 5, "name": "camp"},
+	"5": {"id": 5, "timer": 0, "value": 0, "name": "temple"},
+	"6": {"id": 6, "timer": 0, "value": 0, "name": "hollow"},
+	"7": {"id": 7, "timer": 0, "value": 0, "name": "estate"},
+	"8": {"id": 8, "timer": 0, "value": 0, "name": "overlook"},
+	"9": {"id": 9, "timer": 0, "value": 0, "name": "ascent"}
 };
