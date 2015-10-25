34cbd783485e8c14a6738f83b1ca38fc435ccdd1
Jason Rushton <github.com@schtuph.com>
2015-10-25 15:50:59 -0600

    add new 2.0 objectives

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 module.exports = {
-	langs: require('./data/langs'),
-	worlds: require('./data/world_names'),
-	objective_names: require('./data/objective_names'),
-	objective_types: require('./data/objective_types'),
-	objective_meta: require('./data/objective_meta'),
-	objective_labels: require('./data/objective_labels'),
-	objective_map: require('./data/objective_map'),
+    langs: require('./data/langs'),
+    worlds: require('./data/world_names'),
+    objectives_v2: require('./data/objectives_v2'),
+    objective_names: require('./data/objective_names'),
+    objective_names: require('./data/objective_names'),
+    objective_types: require('./data/objective_types'),
+    objective_meta: require('./data/objective_meta'),
+    objective_labels: require('./data/objective_labels'),
+    objective_map: require('./data/objective_map'),
 };
