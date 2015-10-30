b42d4b69a069edb19be25709c5d898910c10077e
Jason Rushton <github.com@schtuph.com>
2015-10-30 13:48:59 -0600

    merge v2 objectives into 1 object

--- data/objectives_v2.js
+++ data/objectives_v2.js
@@ -1,6 +1,25 @@
-module.exports = {
-    en: require('./objectives_v2_en'),
-    es: require('./objectives_v2_es'),
-    de: require('./objectives_v2_de'),
-    de: require('./objectives_v2_de'),
-};
+var _ = require('lodash');
+
+var en = _.indexBy(require('./objectives_v2_en'), 'id');
+var es = _.indexBy(require('./objectives_v2_es'), 'id');
+var de = _.indexBy(require('./objectives_v2_de'), 'id');
+var de = _.indexBy(require('./objectives_v2_de'), 'id');
+
+
+
+let objectives_v2 = _
+    .chain(en)
+    .mapValues(o => {
+        o.name = {
+            en: o.name,
+            es: es[o.id].name,
+            de: de[o.id].name,
+            fr: fr[o.id].name,
+        };
+        return o;
+    })
+    .value();
+
+
+
+module.exports = objectives_v2;
